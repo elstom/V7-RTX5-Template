2c89d135f6212902122995ea3593431340b5415c
elstom <amcvysa@gmail.com>
2024-10-23 15:33:10 +0800

    Initial commit

---
+++ Project/MDK-ARM(AC6)/RTE/_Flash/RTE_Components.h
@@ -0,0 +1,30 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'project' 
+ * Target:  'Flash' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32h7xx.h"
+
+/*  ARM::CMSIS:RTOS2:Keil RTX5:Source:5.5.0 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+/*  Keil.ARM Compiler::Compiler:Event Recorder:DAP:1.4.0 */
+#define RTE_Compiler_EventRecorder
+          #define RTE_Compiler_EventRecorder_DAP
+/*  Keil::Device:STM32Cube Framework:STM32CubeMX:1.0.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+
+
+#endif /* RTE_COMPONENTS_H */
